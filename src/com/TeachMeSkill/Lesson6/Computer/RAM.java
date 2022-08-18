d69529b19ad77fd2f70095d6de89aa5d2e1c3786
Ozb1 <108549381+Ozb1@users.noreply.github.com>
2022-08-18 19:18:23 +0300

    Lesson6 (#8)
    * Lesson 5
    
    * Lesson 4
    
    * Lesson 4
    
    * Lesson 6

---
+++ src/com/TeachMeSkill/Lesson6/Computer/RAM.java
@@ -0,0 +1,15 @@
+package com.TeachMeSkill.Lesson6.Computer;
+
+public class RAM {
+    String name = "ModelRam";
+    int volume = 16000;
+    public RAM()
+    {
+
+    }
+    public RAM(String name, int volume)
+    {
+        this.name = name;
+        this.volume = volume;
+    }
+}
