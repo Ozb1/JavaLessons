47adebcc24422798d13537d193f84f9a86e2ef9d
Vitalya <Vitalik.Ozb@yandex.by>
2022-09-04 21:29:30 +0300

    Lesson17

---
+++ src/com/TeachMeSkill/Lesson17/Main.java
@@ -0,0 +1,13 @@
+package src.com.TeachMeSkill.Lesson17;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        IpAdressValidService ipAdressValidService = new IpAdressValidService();
+        System.out.println("Enter IP - adress:");
+        Scanner scanner = new Scanner(System.in);
+        String ipAdress = scanner.nextLine();
+        System.out.println(ipAdressValidService.validate(ipAdress));
+    }
+}
